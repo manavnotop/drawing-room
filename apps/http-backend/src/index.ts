7def6099dd057a6d8411945c120a273a6c3a5a13
manavnotop <manuagarwal1443@gmail.com>
2025-02-10 03:17:19 +0530

    Add: auth middleware in http-server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,35 @@
 import express from 'express';
+import { JWT_SECRET } from './config';
+import jwt from 'jsonwebtoken';
+import { middleware } from './middleware';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post("/signin", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/signup", (req, res) => {
+  res.send("Hello World!");
+
+  const userId = 1;
+  const token = jwt.sign({ 
+    userId 
+  }, JWT_SECRET);
+
+  res.json({ 
+    token 
+  });
+}); 
+
+
+app.post("/room", middleware, (req, res) => {
+  //db call
+  res.send("Hello World!");
 });
 
 app.listen(3001, () => {
