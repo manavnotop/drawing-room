5cc390e166009e6f1ec5113e48018c079fc15195
manavnotop <manuagarwal1443@gmail.com>
2025-02-11 14:42:58 +0530

    Add: Common package to import zod types

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import { middleware } from './middleware';
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateUserSchema, SignInSchema, CreateRoomSchema} from '@repo/common/types';
 
 const app = express();
 
@@ -9,13 +10,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signup", (req, res) => {
+
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success) {
+    res.status(400).json({
+      error: "Invalid data",
+    });
+    return;
+  }
+  
   res.json({
     userId: 1,
   });
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signin", (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+  if(!data.success) {
+    res.status(400).json({
+      error: "Invalid data",
+    });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign({ 
     userId 
@@ -28,6 +45,14 @@ app.post("/signup", (req, res) => {
 
 
 app.post("/room", middleware, (req, res) => {
+  
+  const data = CreateRoomSchema.safeParse(req.body);
+  if(!data.success) {
+    res.status(400).json({
+      error: "Invalid data",
+    });
+    return;
+  }
   //db call
   res.json({
     roomId: "123",
